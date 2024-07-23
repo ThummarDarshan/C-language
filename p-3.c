33bc104add1b2a278622d95693ca0ba5f6f01ce3
ThummarDarsahn <darshantce.059@gmail.com>
2024-07-23 09:50:28 +0530

    Add files via upload

---
+++ p-3.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main()
+{
+    int p=50000,r=5.5,t=3,interst;
+    interst=p*r*t/100;
+    printf("Total interst is=%d",interst);
+    printf("\nNAME = Thummar Darshan ");
+    printf("\n ID=24TCEVT0");
+}
+
