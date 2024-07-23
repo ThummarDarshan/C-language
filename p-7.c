e0ddf1dbffa677c9378f38c42f6826d2b50eb839
ThummarDarsahn <darshantce.059@gmail.com>
2024-07-23 10:45:57 +0530

    Add files via upload

---
+++ p-7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int raman,suman,rbunngalow=1200000,rplot=600000,rcar=300000,sapartment=1100000,shotel=800000,scar=800000;
+    raman=rbunngalow+rplot+rcar;
+    suman=sapartment+shotel+scar;
+    printf("\n total ramna properites=%d",raman);
+    printf("\n total suman properites=%d",suman);
+    if(suman>raman)
+    {
+        printf("\nsuman has more properties then raman");
+    }
+    else if(suman=raman)
+    {
+        printf("\nsuman and raman has equal properties");
+    }
+    else
+    {
+          printf("\nsuman has more properties then raman");
+    }
+     printf("\nNAME = Thummar Darshan ");
+    printf("\n ID=24TCEVT0");
+}
