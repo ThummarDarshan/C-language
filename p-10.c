3d9d8c44d2c45927ac7b680369c077338338455e
ThummarDarsahn <darshantce.059@gmail.com>
2024-07-25 15:45:58 +0530

    Add files via upload

---
+++ p-10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int sec;
+    printf("Enter the second=");
+    scanf("%d",&sec);
+    do
+    {
+        printf("\n %d",sec);
+        sec--;
+    }while(sec>=0);
+     printf("\nNAME = Thummar Darshan ");
+    printf("\n ID=24TCEVT0");
+}
