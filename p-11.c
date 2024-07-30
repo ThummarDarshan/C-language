e5ecb8681e434a5af3a79ba5567ece1e6003a2b7
ThummarDarsahn <darshantce.059@gmail.com>
2024-07-30 11:03:11 +0530

    Add files via upload

---
+++ p-11.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int userpick,compick,totalpick=21;
+    printf("plese pick strick 1 to 5 strick from 21");
+    /*printf("pick up your sticks from 21 strick=");
+    scanf("%d",&userpick);*/
+
+    while(totalpick>=1)
+    {
+
+        printf("\npick up your sticks from %d strick=",totalpick);
+        scanf("%d",&userpick);
+        if(userpick<0 || userpick>5 )
+        {
+            printf("invalide pick");
+            continue;
+        }
+        compick=5-userpick;
+        printf("\ncomputer pick =%d",compick);
+        totalpick=totalpick-5;
+        //  continue;
+    }
+    if(totalpick=1)
+    {
+        printf("\nyou are lose");
+
+    }
+
+
+}
