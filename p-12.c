4fa52fd7e769926949e051c8e10b4f1ab566b7d5
ThummarDarsahn <darshantce.059@gmail.com>
2024-08-01 15:32:42 +0530

    Add files via upload

---
+++ p-12.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int cost,i;
+    for(cost=0,i=1 ; i<=100 ; i++)
+    {
+        if(i%2==0)
+        {
+            cost+=200;
+        }
+    }
+    printf("\n Your total cost=%d",cost);
+    printf("\nNAME = Thummar Darshan ");
+    printf("\n ID=24TCEVT0");
+
+}
