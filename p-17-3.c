beb1b6c617cb860b29857034ecf96619e9a43233
ThummarDarsahn <darshantce.059@gmail.com>
2024-09-03 17:25:37 +0530

    Add files via upload

---
+++ p-17-3.c
@@ -0,0 +1,44 @@
+/*program by manush desai cspit ce
+aim:-booking tickets code
+date:-11 aug*/
+
+#include <stdio.h>
+int main()
+{
+int seats[5][10]={0},number,i,row,col,j,k;
+  printf("enter the number of tickits you want to book\n");
+  scanf("%d",&number);
+    for(k=0;k<number;k++)
+    {
+        printf("enter the row first and then seat which you are going to booked\n");
+        scanf("%d",&row);
+        scanf("%d",&col);
+        row=row-1;
+        col=col-1;
+
+            if (row >= 0 && row < 5 && col >= 0 && col < 10)
+                {
+                    if (seats[row][col] == 0)
+                        {
+                            seats[row][col] = 1;
+                        }
+                }
+            else
+                {
+                printf("invalid seat number");
+                }
+        for(i=0;i<5;i++)
+            {
+                for(j=0;j<10;j++)
+                    {
+                        if(seats[i][j]==1)
+                        {
+                            printf(" x ");
+                        }
+                        else
+                        printf(" 0 ");
+                    }
+                    printf("\n");
+        }
+}
+}
